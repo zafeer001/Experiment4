8adecb04eb7d5798c24f9208f00d43db93d462fe
zafeer001 <157670271+zafeer001@users.noreply.github.com>
2024-04-17 21:00:20 +0530

    Create Quotientremainder.java

---
+++ Quotientremainder.java
@@ -0,0 +1,14 @@
+public class QuotientAndRemainder { 
+
+	public static void main(String[] args) 
+	{ 
+
+		int dividend = 556, divisor = 9; 
+
+		int quotient = dividend / divisor; 
+		int remainder = dividend % divisor; 
+
+		System.out.println("The Quotient is = " + quotient); 
+		System.out.println("The Remainder is = " + remainder); 
+	} 
+} 
